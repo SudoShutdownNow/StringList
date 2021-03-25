85db0a13cd37e3724d0d4eb713e7f4c15c11d35e
SudoShutdownNow <zhaozeyu8888@outlook.com>
2021-03-25 20:39:32 +0800

    查找部分写完了

--- StringList/源.cpp
+++ StringList/源.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 #pragma warning(disable : 4996)
 using namespace std;
 #define MAX_WORD_LENGTH 20
@@ -9,11 +10,12 @@ struct WordNode {
 	char word[MAX_WORD_LENGTH];
 	WordNode* next;
 	bool isEmpty;
-	WordNode() :next(NULL), isEmpty(true) {};
-	WordNode(char* myword):isEmpty(false),next(NULL) {
+	bool labeled;
+	WordNode() :next(NULL), isEmpty(true),labeled(false) {};
+	WordNode(char* myword):isEmpty(false),next(NULL),labeled(false) {
 		strcpy(word, myword);
 	}
-	WordNode(char* myword,WordNode* mynext) :isEmpty(false),next(mynext) {
+	WordNode(char* myword,WordNode* mynext) :isEmpty(false),next(mynext),labeled(false) {
 		strcpy(word, myword);
 	}
 };
@@ -52,13 +54,85 @@ int getWordFromFile(char* filename,WordNode*& list)//从文件读取单词
 		append(list, newword);//将读取到的单词追加在链表末尾
 	}
 	append(list, newword);
+	return OK;
+}
+
+int getWordFromInput(WordNode*& list)//从键盘读取单词
+{
+	char newword[MAX_WORD_LENGTH];
+	while (cin>>newword)//从头读取文件直到文件尾
+	{
+		if (strcmp(newword, "`") == 0)
+		{
+			return OK;
+		}
+		append(list, newword);//将读取到的单词追加在链表末尾
+	}
+	//append(list, newword);
+	return OK;
 }
 
 int explore(WordNode* List)
 {
 	for (WordNode* p = List; p->next != NULL; p = p->next)
 	{
-		cout << p->word << " ";
+		if (p->labeled == true)
+		{
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED);//把控制台设置成红色
+			//cout << "***";
+			cout << p->word <<" ";
+
+		}
+		else
+		{
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED |
+				FOREGROUND_GREEN | FOREGROUND_BLUE);//控制台设置为白色
+			cout << p->word << " ";
+		}
+
+	}
+	return OK;
+}
+
+int seek(WordNode* paper, WordNode* target)
+{
+	WordNode* piter, * titer;
+	titer = target;
+	WordNode* label = NULL;
+	for (piter = paper; piter->next != NULL; piter = piter->next)//piter从第一个单词开始遍历Paper
+	{
+		if (strcmp(titer->word, piter->word) == 0)//暂存相同部分开始的位置
+		{
+			label = piter;
+		}
+		while (strcmp(titer->word, piter->word) == 0)
+		{
+			if (titer->next == NULL)//一直匹配到目标串的末尾了，说明匹配中了
+			{
+				WordNode* oper = label;
+				for (oper = label; oper != piter; oper = oper->next)//oper在paper上从label位置开始，直到当前位置结束
+				{
+					cout << oper->word << endl;
+					oper->labeled = true;//将标记改为存在
+				}
+				oper->labeled = true;//同理把最后一个处理掉
+
+				titer = target;//给titer归位
+				//return OK;
+			}
+
+			if (piter->next != NULL)
+			{
+				//如果目标串没有到头，目标串和文本串指针同时向前移动
+				titer = titer->next;
+				piter = piter->next;
+			}
+			else
+			{
+				return OK;
+			}
+			
+		}
 	}
 	return OK;
 }
@@ -66,13 +140,23 @@ int explore(WordNode* List)
 void testmain()
 {
 	char filename[20];
+	cout << "请输入文本地址：";
 	cin >> filename;
-
+	cout << "请输入要查找的串：";
+	WordNode* target = new WordNode;
+	getWordFromInput(target);
 	WordNode* paper = new WordNode;
 	getWordFromFile(filename, paper);
+	seek(paper, target);
 	explore(paper);
 	system("pause");
 
+}
+
+void testmain1()
+{
+	WordNode* input = new WordNode;
+	getWordFromInput(input);
 }
 
 void main()
